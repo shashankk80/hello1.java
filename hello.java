3a1e85e13ecb26fab307258c17880fa3416ec98c
shashankk80 <shashankk8014@gmail.com>
2023-06-19 15:36:26 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,7 @@
-public class hello
-public static void main(String[] Args)
+class Test
 {
-System.out.println("hello world");
-}
+    public static void main(String []args)
+    {
+        System.out.println("My First Java Program.");
+    }
+};
